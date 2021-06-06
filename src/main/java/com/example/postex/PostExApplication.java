ac8ece5cd3a55363e5ddc8030fd469fd87bf3b14
everglow <78077569+okchanho@users.noreply.github.com>
2021-06-07 08:46:15 +0900

    스프링부트 Post예제

---
+++ src/main/java/com/example/postex/PostExApplication.java
@@ -0,0 +1,13 @@
+package com.example.postex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postex/PostExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
