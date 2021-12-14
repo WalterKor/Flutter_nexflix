672fc944e0aea526831db1813568eba236aa1632
everglow <78077569+okchanho@users.noreply.github.com>
2021-12-14 23:11:02 +0900

    update

--- NetfilxClone/lib/main.dart
+++ NetfilxClone/lib/main.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/screen/home_screen.dart';
+import 'package:netflix_clone/screen/more_screen.dart';
 import 'package:netflix_clone/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget{
-  _MyAppState createState()=> _MyAppState();
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>{
+class _MyAppState extends State<MyApp> {
   // TabController controller;
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title:"WalterFlix",
+      title: "WalterFlix",
       theme: ThemeData(
           brightness: Brightness.dark,
           primaryColor: Colors.black,
-          accentColor: Colors.white
-      ),
+          accentColor: Colors.white),
       home: DefaultTabController(
         length: 4,
         child: Scaffold(
@@ -26,16 +26,22 @@ class _MyAppState extends State<MyApp>{
             physics: NeverScrollableScrollPhysics(),
             children: <Widget>[
               HomeScreen(),
-              Container(child: Center(child: Text('search'),),),
-              Container(child: Center(child: Text('save'),),),
-              Container(child: Center(child: Text('more'),),)
+              Container(
+                child: Center(
+                  child: Text('search'),
+                ),
+              ),
+              Container(
+                child: Center(
+                  child: Text('save'),
+                ),
+              ),
+              MoreScreen(),
             ],
           ),
           bottomNavigationBar: Bottom(),
         ),
-        ),
+      ),
     );
   }
 }
-
-
--- NetfilxClone/lib/widget/carousel_slider.dart
+++ NetfilxClone/lib/widget/carousel_slider.dart
@@ -1,10 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix_clone/model/model_movie.dart';
+import 'package:netflix_clone/screen/detail_screen.dart';
 
 class CarouselImage extends StatefulWidget {
   final dynamic movies;
-
   CarouselImage({this.movies});
 
   _CarouselImageState createState() => _CarouselImageState();
@@ -22,7 +22,7 @@ class _CarouselImageState extends State<CarouselImage> {
   void initState() {
     super.initState();
     movies = widget.movies;
-    images = movies.map((m) => Image.asset('./images/' + m.poster)).toList();
+    images = movies.map((m) => Image.network(m.poster)).toList();
     keywords = movies.map((m) => m.keyword).toList();
     likes = movies.map((m) => m.like).toList();
     _currentKeyword = keywords[0];
@@ -113,7 +113,15 @@ class _CarouselImageState extends State<CarouselImage> {
                     children: <Widget>[
                       IconButton(
                         icon: Icon(Icons.info),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute<Null>(
+                              fullscreenDialog: true,
+                              builder: (BuildContext context) {
+                                return DetailScreen(
+                                  movie: movies[_currentPage],
+                                );
+                              }));
+                        },
                       ),
                       Text(
                         '정보',
@@ -126,7 +134,7 @@ class _CarouselImageState extends State<CarouselImage> {
             ),
           ),
 
-          /*========== 도트 ==========*/
+          /*========== 인디케이터 ==========*/
           Container(
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
